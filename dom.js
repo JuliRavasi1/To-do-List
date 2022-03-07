dab422a662a7c66ba0dc2dd0560f384ef993ea27
JuliRavasi1 <juli.ravasi00@gmail.com>
2022-03-07 22:58:44 +0100

    dom hecho (entrega miercoles)

---
+++ dom.js
@@ -0,0 +1,19 @@
+let tabla = document.querySelector("#tabla");
+
+let cuerpo = document.createElement("tbody");
+let tr;
+
+tareas.forEach(tarea => {
+    tr = document.createElement("tr");
+    // tr.setAttribute("class", "clasequequiero")
+    tr.innerHTML =
+        `
+    <th>${tarea.descripcion}</th>
+    <th>${tarea.hora}</th>
+    <th>${tarea.prioridad}</th>
+    `
+    cuerpo.appendChild(tr);
+});
+
+tabla.appendChild(cuerpo);
+
