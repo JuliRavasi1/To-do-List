53ac245ee4f837ed1ea313abb8b845eebb034833
JuliRavasi1 <juli.ravasi00@gmail.com>
2022-03-09 11:19:55 +0100

    eventos por la mitad

--- dom.js
+++ dom.js
@@ -1,28 +1,91 @@
-//lista imprimiendo tareas 
+/////////////////////// DOM DESAFIO ///////////////////////
 
-let tabla = document.querySelector("#tabla");
+//lista imprimiendo tareas //////////
 
-let cuerpo = document.createElement("tbody");
-let tr;
+// let tabla = document.querySelector("#tabla");
 
-tareas.forEach(tarea => {
-    tr = document.createElement("tr");
-    // tr.setAttribute("class", "clasequequiero")
-    tr.innerHTML =
-        `
-    <th>${tarea.descripcion}</th>
-    <th>${tarea.hora}</th>
-    <th>${tarea.prioridad}</th>
-    `
-    cuerpo.appendChild(tr);
-});
+// let cuerpo = document.createElement("tbody");
+// let tr;
 
-tabla.appendChild(cuerpo);
+// tareas.forEach(tarea => {
+//     tr = document.createElement("tr");
+//     tr.innerHTML =
+//         `
+//     <th>${tarea.descripcion}</th>
+//     <th>${tarea.hora}</th>
+//     <th>${tarea.prioridad}</th>
+//     `
+//     cuerpo.appendChild(tr);
+// });
 
-// contador de tareas 
+// tabla.appendChild(cuerpo);
 
-let tareasPend = document.querySelector("#pendingTask");
+// contador de tareas /////////
 
-tareasPend.innerHTML = 
-`You have ${tareas.length} pending tasks.`
+// let tareasPend = document.querySelector("#pendingTask");
 
+// tareasPend.innerHTML = 
+// `You have ${tareas.length} pending tasks`
+
+/////////////////////// EVENTOS CLASE ///////////////////////
+
+// function init (){
+
+// }
+
+// function programarBotones (){
+//     programarBotonSubmit();
+// }
+// function programarBotonSubmit (){
+    // boton.onclick = ()=> { }
+    // }
+
+    // function programarInput(){
+    //     const input = document.querySelector("#inputTarea");
+    //     input.addEventListener("focus", (e)=> {
+    //         if (e.target.value === "Add your new to-do"){
+    //             e.target.value = " ";
+    //         }
+    //     });
+    //     input.addEventListener("blur", (e)=> {
+    //         if (e.target.value === " "){
+    //             e.target.value = "Add your new to-do";
+    //         }
+    //     });
+    // }
+
+/////////////////////// EVENTOS DESAFIO ///////////////////////
+
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+const boton = document.querySelector("#botonSubmit");
+
+    boton.addEventListener("click", () => {
+        const text = input.value;
+
+        const li = document.createElement("li");        
+        const p = document.createElement("p");
+        
+        p.textContent = text;
+        
+        li.appendChild(p); 
+        li.appendChild(trashButton());
+        ul.appendChild(li);
+
+        input.value = " ";
+    });
+    
+
+    function trashButton () {
+        const button = document.createElement("button");
+        button.className = ("delete");
+        
+        button.innerHTML = 
+        '<i class="fas fa-trash"></i>'
+
+        button.addEventListener("click", (e)=> {
+            const item = e.target.parentElement;
+            ul.removeChild(item);
+        });
+        return button;
+    }
