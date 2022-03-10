d66f6f9b3b1b589896777df7696b58b5cb0b1ecf
JuliRavasi1 <juli.ravasi00@gmail.com>
2022-03-10 16:59:20 +0100

    eventos. Faltan botones y que no se sumen las tareas

--- dom-eventos.js
+++ dom-eventos.js
@@ -56,13 +56,63 @@
 
 /////////////////////// EVENTOS DESAFIO ///////////////////////
 
-    let listaLista = [
-        {
-            descripcion: "",
-            hora: '',
-            fecha: "", 
+
+class Dulce {
+    constructor(nombre, precio, images) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.images = images;
+    }
+}
+
+// declaracion de arrays
+// let nuevoDulce;
+const dulces = [];
+// let carrito = [];
+dulces.push(new Dulce("Dos Corazones", 80, "./images/Dos-Corazones.jpg"));
+dulces.push(new Dulce("Mogul Gomitas", 60, "./images/Mogul-Gomitas.jpg"));
+dulces.push(new Dulce("Ferrero Rocher", 250, "./images/Ferrero-Rocher.jpg"));
+dulces.push(new Dulce("Shot Bloque Chocolate", 160, "./images/Shot-Bloque-Chocolate.webp"));
+dulces.push(new Dulce("Kinder Chocolate", 255, "./images/Kinder-Chocolate.jpeg"));
+dulces.push(new Dulce("Jorgito Alfajor", 85, "./images/Jorgito-Alfajor.jpg"));
+dulces.push(new Dulce("Milka Chocolate", 310, "./images/Milka-Chocolate.png"));
+dulces.push(new Dulce("Pepitos Alfajor", 150, "./images/Pepitos-Alfajor.jpg"));
+dulces.push(new Dulce("Bon o Bon", 55, "./images/Bon-o-Bon.jpg"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Lista {
+        constructor (descripcion, hora, fecha){ 
+            this.descripcion = descripcion;
+            this.hora = hora; 
+            this.fecha = fecha;
         }
-    ]
+    }
+
+    let listas = []; 
 
     function init(){
         guardarTareaAccion();
@@ -84,11 +134,9 @@
         const horaInput = document.querySelector("#time").value;
         const fechaInput = document.querySelector("#date").value;
 
-        // console.log (tareaInput)
-        // console.log (horaInput)
-        // console.log (fechaInput)
+        listas.push (new Lista(`${tareaInput}`, `${horaInput}`, `${fechaInput}`));
 
-        if (tareaInput.trim() === "" || horaInput.trim() === '' || fechaInput.trim() === ""){
+        if (tareaInput.trim() === "" || horaInput.trim() === "" || fechaInput.trim() === ""){
             tareaInput.value = "";
             horaInput.value = "";
             fechaInput.value = "";
@@ -100,18 +148,29 @@
             const fechaFinal = new Date(fechaFormat[0], fechaFormat[1]-1, fechaFormat[2]); //[0] es el año, [1] es el mes y se le resta 1 porque va de 0 a 11, el [2] es el dia. Por ejemplo, elijo ENERO, entonces es 1-1=0 y 0 es ENERO; si pongo JUNIO es 7-1=6 y 6 es JUNIO. Eso es por el OBJETO DATE. 
 
             const miliSeg = today.getTime();
-            const segFinal = fechaFinal.getTime(); //es mas preciso usar milisegundos para comparacion de fechas
+            const segFinal = fechaFinal.getTime(); //es mas preciso usar milisegundos para comparacion de fechas.
 
             if(segFinal < today){
                 alert ("ingresa fecha posterior");
             }else {
-                const tarea = {
-                    descripcion: tareaInput,
-                    hora: horaInput,
-                    fecha: `${fechaFormat[2]}/${fechaFormat[1]}/${fechaFormat[0]}`
-                } 
-                listaLista.push(tarea);
-            }
+                crearTable(); 
+            } 
         }
-        console.table (listaLista)
+    }
+
+
+    const table = document.querySelector("table");
+    const body = document.querySelector("tbody");
+
+
+    function crearTable(){
+        listas.forEach(tarea => {
+            let tr = document.createElement("tr");
+            tr.innerHTML = 
+            `<td>${tarea.descripcion}</td>
+            <td>${tarea.hora}</td>
+            <td>${tarea.fecha}</td>`;
+
+            body.appendChild(tr);
+        }); 
     }
