e33b446147e1260de09645e1b7e88d4bd41ec4f0
JuliRavasi1 <juli.ravasi00@gmail.com>
2022-03-12 17:55:23 +0100

    eventos y dom

---
+++ eventos-dom.js
@@ -0,0 +1,60 @@
+// que al hacer click en SAVE se guarden los datos de los inputs en un array DONE
+// imprimir ese array en un tr y td 
+
+
+class Lista {
+    constructor(descripcion, fecha, hora) {
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+}
+
+const listas = [];
+
+const btnSave = document.querySelector("#botonSubmit");
+
+btnSave.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    guardarTareas();
+});
+
+function guardarTareas() {
+    const descripcionInput = document.querySelector("#form1").value;
+    const fechaInput = document.querySelector("#date").value;
+    const horaInput = document.querySelector("#time").value;
+
+    if (descripcionInput.trim() === "" || fechaInput.trim() === "" || horaInput.trim() === "") {
+        alert("COMPLETE TODOS LOS CAMPOS");
+    } else {
+        listas.push(new Lista(`${descripcionInput}`, `${fechaInput}`, `${horaInput}`));
+        imprimirTareas(listas);
+    }
+    document.getElementById("formulario").reset();
+}
+
+function imprimirTareas(listas) {
+    const tableBody = document.querySelector("tbody");
+    tableBody.textContent = "";
+
+    let tr;
+    listas.forEach(tarea => {
+        tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>${tarea.descripcion}</td>
+            <td>${tarea.fecha}</td>
+            <td>${tarea.hora}</td>
+            <button id="${tarea.descripcion}" class="btn btn-danger" type="submit">Delete</button>
+            <button id="${tarea.descripcion}" class="btn btn-dark ms-1" type="submit">Finished</button>
+        `
+        tableBody.appendChild(tr);
+        
+        const btnEliminarAll = document.querySelector ("#btnDeleteAll");
+        btnEliminarAll.onclick = () => {
+            tableBody.remove(tr);
+        };
+            return btnEliminarAll;
+    });
+}
+
