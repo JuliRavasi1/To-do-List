f876179bac90088e848cf40fd30430a6623d3934
JuliRavasi1 <juli.ravasi00@gmail.com>
2022-03-18 14:49:58 +0100

    storage

--- eventos-dom.js
+++ eventos-dom.js
@@ -9,20 +9,21 @@ class Lista {
 let listas = [];
 
 const btnSave = document.querySelector("#botonSubmit");
+let tableBody = document.querySelector("#contenedorBody");
 
-storageTareasPrecarga(); 
+storageTareasPrecarga();
 
 btnSave.addEventListener("click", (e) => {
     e.preventDefault();
-    
+
     guardarTareas();
 });
 
 function guardarTareas() {
-    const descripcionInput = document.querySelector("#form1").value;   
+    const descripcionInput = document.querySelector("#form1").value;
     const fechaInput = document.querySelector("#date").value;
     const horaInput = document.querySelector("#time").value;
-    
+
     if (descripcionInput.trim() === "" || fechaInput.trim() === "" || horaInput.trim() === "") {
         alert("COMPLETE TODOS LOS CAMPOS");
     } else {
@@ -33,61 +34,61 @@ function guardarTareas() {
 }
 
 function imprimirTareas(listas) {
-    const tableBody = document.querySelector("#contenedorBody");
+    let tableBody = document.querySelector("#contenedorBody");
     tableBody.textContent = "";
-    
+
     let tr;
-    listas.forEach(tarea => {
+    listas.forEach((tarea) => {
         tr = document.createElement("tr");
         tr.innerHTML = `
         <td>${tarea.descripcion}</td>
         <td>${tarea.fecha}</td>
         <td>${tarea.hora}</td>
         <button id="hecho${tarea.descripcion}" class="btn btn-dark ms-1" type="submit">Finished</button>
-        `
+        `;
         tableBody.appendChild(tr);
-        
-        const btnEliminarAll = document.querySelector ("#btnDeleteAll");
-        btnEliminarAll.onclick = () => {
-            tableBody.remove(tr);
-            localStorage.clear();
-        };
-        return btnEliminarAll;
     });
     storageTareas();
 }
 
-function storageTareas(){
-    localStorage.setItem("tarea", JSON.stringify (listas));
+const btnEliminarAll = document.querySelector("#btnDeleteAll");
+btnEliminarAll.onclick = () => {
+    listas = [];
+    tableBody.textContent = "";
+    localStorage.clear();
+};
+
+function storageTareas() {
+    localStorage.setItem("tarea", JSON.stringify(listas));
 }
 
-function storageTareasPrecarga(){
-    if (localStorage.getItem("tarea") !== null){ 
-        listas = JSON.parse(localStorage.getItem("tarea")); 
+function storageTareasPrecarga() {
+    if (localStorage.getItem("tarea") !== null) {
+        listas = JSON.parse(localStorage.getItem("tarea"));
     }
-    reimprimirTareas(); 
+    reimprimirTareas();
 }
 
-function reimprimirTareas(){
+function reimprimirTareas() {
     const tableBody = document.querySelector("#contenedorBody");
     tableBody.textContent = "";
-    
+
     let tr;
-    listas.forEach(tarea => {
+    listas.forEach((tarea) => {
         tr = document.createElement("tr");
         tr.innerHTML = `
         <td>${tarea.descripcion}</td>
         <td>${tarea.fecha}</td>
         <td>${tarea.hora}</td>
         <button id="${tarea.descripcion}" class="btn btn-dark ms-1" type="submit">Finished</button>
-        `
+        `;
         tableBody.appendChild(tr);
-        
-        const btnEliminarAll = document.querySelector ("#btnDeleteAll");
+
+        const btnEliminarAll = document.querySelector("#btnDeleteAll");
         btnEliminarAll.onclick = () => {
             tableBody.remove(tr);
             localStorage.clear();
-        }
+        };
         return btnEliminarAll;
     });
 }
