04990a5acf16abc0a1a73881047ac04415412e57
JuliRavasi1 <juli.ravasi00@gmail.com>
2022-04-02 10:41:49 +0200

    agregue toastify

--- eventos-dom.js
+++ eventos-dom.js
@@ -1,9 +1,10 @@
 class Lista {
-    constructor(descripcion, fecha, hora, estado) {
+    constructor(descripcion, fecha, hora, estado, id) {
         this.descripcion = descripcion;
         this.fecha = fecha;
         this.hora = hora;
-        this.estado = estado; 
+        this.estado = estado;
+        this.id = id;
     }
 }
 
@@ -11,26 +12,42 @@ let listas = [];
 
 // busca una tarea que recibe por parametro en el storage
 // devuelve boolean si encuentra o no la tarea 
-function buscarTarea(valor){
+function buscarTarea(valor) {
     let valorGuardado = getTareas();
-    for(const tarea of valorGuardado){
-        if (tarea.descripcion == valor){
-            return true; 
-        } 
+    for (const tarea of valorGuardado) {
+        if (tarea.id == valor) {
+            return true;
+        }
     }
-    return false; 
+    return false;
 }
 
-function getTareas() { 
+// guarda las tareas en el storage
+function setTareas(lista) {
+    localStorage.setItem("tarea", JSON.stringify(lista));
+}
+
+// me trae las tareas del storage
+function getTareas() {
     let listaTarea = JSON.parse(localStorage.getItem("tarea"));
-    if (listaTarea == null || listaTarea == undefined){
-        return []; 
+    if (listaTarea == null || listaTarea == undefined) {
+        return [];
     }
     return listaTarea;
 }
 
-function setTareas(lista) {
-    localStorage.setItem("tarea", JSON.stringify(lista));
+//
+function setId(id) {
+    localStorage.setItem("Id", JSON.stringify(id));
+}
+
+//
+function getId() {
+    let id = JSON.parse(localStorage.getItem("Id"));
+    if (id == null || id == undefined) {
+        return 0;
+    }
+    return id;
 }
 
 const btnSave = document.querySelector("#botonSubmit");
@@ -46,18 +63,20 @@ btnSave.addEventListener("click", (e) => {
 });
 
 function guardarTareas() {
+    let id = getId();
+    id++;
     const descripcionInput = document.querySelector("#form1").value;
     const fechaInput = document.querySelector("#date").value;
     const horaInput = document.querySelector("#time").value;
 
     if (descripcionInput.trim() === "" || fechaInput.trim() === "" || horaInput.trim() === "") {
-            Swal.fire({
-                title: "CAMPOS INCORRECTOS",
-                text: "complete todos los campos para continuar",
-                icon: "error"
-            });
+        Swal.fire({
+            title: "CAMPOS INCORRECTOS",
+            text: "complete todos los campos para continuar",
+            icon: "error"
+        });
     } else {
-        if (buscarTarea(descripcionInput)){
+        if (buscarTarea(id)) {
             Swal.fire({
                 title: "TAREA REPETIDA",
                 text: "ingrese una tarea diferente",
@@ -65,9 +84,10 @@ function guardarTareas() {
             });
         } else {
             let valoresStorage = getTareas();
-            valoresStorage.push(new Lista(`${descripcionInput}`, `${fechaInput}`, `${horaInput}`, false));
-            
+            valoresStorage.push(new Lista(`${descripcionInput}`, `${fechaInput}`, `${horaInput}`, false, id));
+
             setTareas(valoresStorage);
+            setId(id);
             imprimirTareas();
         }
     }
@@ -86,20 +106,30 @@ function imprimirTareas() {
         <td>${tarea.descripcion}</td>
         <td>${tarea.fecha}</td>
         <td>${tarea.hora}</td>
-        <button id="hecho${tarea.descripcion}" class="btn btn-dark ms-1" type="submit">Finished</button>
+        <button id="hecho${tarea.id}" class="btn btn-dark ms-1" type="submit">Finished</button>
         `;
 
-        tr.setAttribute("class", `${tarea.descripcion}`);
-        tableBody.appendChild(tr);        
-        const btnDone = document.querySelector(`#hecho${tarea.descripcion}`);
-        
+        tr.setAttribute("class", `done${tarea.id}`);
+        tableBody.appendChild(tr);
+        const btnDone = document.querySelector(`#hecho${tarea.id}`);
+
         btnDone.addEventListener("click", () => {
-            tarea.estado= !tarea.estado;
+            tarea.estado = !tarea.estado;
             if (tarea.estado == true) {
                 btnDone.innerText = "";
                 btnDone.innerText = "Unfinished";
-
                 trDone.setAttribute("style", "text-decoration:line-through");
+
+                Toastify({
+                    text: "Nice! You're finally doing your shit :)",
+                    duration: 3000,
+                    gravity: "top",
+                    position: "right",
+                    style: {
+                        background: "#007F66"
+                    }
+                }).showToast();
+
             } else {
                 btnDone.innerText = "";
                 btnDone.innerText = "Finished";
@@ -108,8 +138,8 @@ function imprimirTareas() {
             setTareas(listas);
         });
 
-        const trDone = document.querySelector(`.${tarea.descripcion}`); 
-        if (tarea.estado == true) { 
+        const trDone = document.querySelector(`.done${tarea.id}`);
+        if (tarea.estado == true) {
             btnDone.innerText = "";
             btnDone.innerText = "Unfinished";
             trDone.setAttribute("style", "text-decoration:line-through");
@@ -128,12 +158,12 @@ btnEliminarAll.onclick = () => {
         confirmButtonText: "SI, ELIMINAR",
         cancelButtonText: "CANCELAR",
         icon: "warning"
-    }).then((respuesta)=>{
-        if (respuesta.isConfirmed){
+    }).then((respuesta) => {
+        if (respuesta.isConfirmed) {
             listas = [];
             tableBody.textContent = "";
             localStorage.clear();
-        }else{
+        } else {
             console.log("no elimino");
         }
     });
